f7f25111c809ef8c99faf44574c12fc324810e1d
Andrii Cherniienko <chern.combat32@gmail.com>
2026-06-20 12:37:32 +0300

    One-liners completed

--- app/main.py
+++ app/main.py
@@ -8,6 +8,7 @@ def format_linter_error(error: dict) -> dict:
         "source": "flake8"
     }
 
+
 def format_single_linter_file(file_path: str, errors: list) -> dict:
 
     return {
@@ -15,6 +16,7 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
         "path": file_path,
         "status": "failed" if errors else "passed"
     }
+
 
 def format_linter_report(linter_report: dict) -> list:
 
